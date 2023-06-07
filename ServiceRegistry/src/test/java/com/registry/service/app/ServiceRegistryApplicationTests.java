d0002b93b4d5a1d665503e219062ca7cc7725cec
subhajit51193 <nnorth87@gmail.com>
2023-06-07 22:21:40 +0530

    service registry created

---
+++ ServiceRegistry/src/test/java/com/registry/service/app/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registry.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
