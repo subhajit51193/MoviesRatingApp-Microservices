9103ead3781cd65e9d89764783f3906935d93a00
subhajit51193 <nnorth87@gmail.com>
2023-06-10 22:50:48 +0530

    working on api gateway config

---
+++ ApiGateway/src/test/java/com/api/gateway/app/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gateway.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
