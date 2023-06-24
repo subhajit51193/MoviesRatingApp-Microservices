9d6ff98b123cfcc12aa4ded3d264737a824c751b
subhajit51193 <nnorth87@gmail.com>
2023-06-24 22:14:29 +0530

    as of now only api service secured

--- UserService/src/main/java/com/user/service/app/config/MyConfig.java
+++ UserService/src/main/java/com/user/service/app/config/MyConfig.java
@@ -1,16 +1,27 @@
 package com.user.service.app.config;
 
+
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.client.RestTemplate;
+
+
 
 @Configuration
 public class MyConfig {
 
+	
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate() {
+		
+		
 		return new RestTemplate();
+		
 	}
+	
+	 
+
 }
